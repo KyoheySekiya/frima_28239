4d911265612978e0490da8bd9a1d6d3ad09d7bc5
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-08-24 12:35:22 +0900

    商品出品画面の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+  def edit
+    
+  end
+  
   def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
   post '/users/sign_up', to:'users#create'
+  get '/items/new', to:'items#new'
   resources :users, only: [:show, :create]
-
+  resources :items, only: [:edit, :show]
 end
