369bb102aec4efa15b263e6a4a818a83ebc554d1
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-08-30 22:33:19 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -21,7 +20,6 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
 
   private
 
